d847e84cfe142ab7f4aab3549f345a6b5b580df5
shaday823-coder <zcarrillor823@inemkennedy.edu.co>
2026-05-21 12:58:07 -0500

    TAREA 4

--- app.py
+++ app.py
@@ -9,32 +9,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
+
+    titulo = "Panel de inicio"
+    usuario = "Zury"
+    mensaje = "Bienvenida a Flask"
    
-    return render_template("index.html")
+    return render_template("index.html",
+                             
+    titulo=titulo,
+    usuario=usuario,
+    mensaje=mensaje)
 
 
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
 
-@app.route("/HOLA")
-def hola():
+
+@app.route("/acerca")
+def acerca():
+    return render_template("acerca.html")
+
+@app.route("/recursos")
+def recursos():
+    recursos = [
+    "Entorno virtual",
+    "Rutas en Flask",
+    "Plantillas HTML",
+    "Variables con Jinja"
+]
+    return render_template("recursos.html",recursos=recursos)
     
-    return render_template("HOLA.html")
-
-@app.route("/ADIOS")
-def adios():
-    
-    return render_template("ADIOS.html")
-
-
-
-
-
-
-@app.route("/LOVE")
-def love():
-    
-    return render_template("LOVE.html")
-
-
 
 
 if __name__ == "__main__":
